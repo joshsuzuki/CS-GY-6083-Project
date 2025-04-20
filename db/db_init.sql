4a5e16ae5cbb32736e268837020093180b4cc547
Joshua Suzuki <jks9908@nyu.edu>
2025-04-20 14:56:24 -0400

    db init update

--- db/db_init.sql
+++ db/db_init.sql
@@ -12,3 +12,35 @@ CREATE TABLE employees (
     current_employee BOOLEAN DEFAULT TRUE,
     hire_date DATETIME DEFAULT NOW()
 );
+
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE groups (
+    group_id INT AUTO_INCREMENT PRIMARY KEY,
+    group_name VARCHAR(50) NOT NULL,
+);
+
+
+DROP TABLE IF EXISTS balances;
+
+CREATE TABLE balances (
+    account INT NOT NULL,
+    entity INT NOT NULL,
+    counterparty INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL,
+    amount DECIMAL(18,2) DEFAULT 0,
+    n_id_updated_by INT NOT NULL,
+    dt_last_updated DATETIME DEFAULT NOW(),
+    PRIMARY KEY (account, entity, counterparty,month,year)
+);
+
+DROP TABLE IF EXISTS employees_groups;
+
+CREATE TABLE employees_groups (
+    group_id INT NOT NULL,
+    employee_id INT NOT NULL,
+    PRIMARY KEY (group_id, employee_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE CASCADE
+);
--- db/tables/employees_groups.sql
+++ db/tables/employees_groups.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS employees_groups;
-
 CREATE TABLE employees_groups (
     group_id INT NOT NULL,
-    employee_id NT NOT NULL,
-    PRIMARY KEY (group_id, employee_id)
+    employee_id INT NOT NULL,
+    PRIMARY KEY (group_id, employee_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE CASCADE
 );
